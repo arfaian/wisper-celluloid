47cedbd1575f5441c1a36741450dc99eaa23b133
Arian Arfaian <arian.arfaian@gmail.com>
2015-04-05 01:56:33 -0400

    do not wrap pre existing actors

--- lib/wisper/celluloid_broadcaster.rb
+++ lib/wisper/celluloid_broadcaster.rb
@@ -3,20 +3,7 @@ require 'celluloid/autostart'
 module Wisper
   class CelluloidBroadcaster
     def broadcast(subscriber, publisher, event, args)
-      Wrapper.new(subscriber).async.broadcast(event, args)
-    end
-
-    class Wrapper
-      include ::Celluloid
-
-      def initialize(subscriber)
-        @subscriber = subscriber
-      end
-
-      def broadcast(event, args)
-        @subscriber.public_send(event, *args)
-        terminate
-      end
+      subscriber.async.public_send(event, *args)
     end
 
     def self.register
